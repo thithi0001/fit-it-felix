56bb45392daab928ed275889fa2f16c416003bf5
thithi0001 <ledinhthi0002@gmail.com>
2026-08-09 16:06:34 +0700

    fixed: express.json consume body

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -3,8 +3,9 @@ import authRoute from "./routes/auth.route.js";
 import { errorMiddleware } from "../../shared/middlewares/error.middleware.js";
 
 const app = express();
-app.use(express.json());
+
 app.use(authRoute);
+// app.use(express.json());
 app.use(errorMiddleware);
 
 export default app;
