b80b5a01f4fdf1016b554264a542f73472c1c1a0
thithi0001 <ledinhthi0002@gmail.com>
2026-08-10 12:34:51 +0700

    shared auth middleware

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import authRoute from "./routes/auth.route.js";
 import { errorMiddleware } from "../../shared/middlewares/error.middleware.js";
+import { authenticateGateway } from "./middlewares/auth.middleware.js";
 
 const app = express();
+const publicRoutes = ["/auth/health", "/auth/login", "/auth/refresh"];
 
+app.use((req, res, next) => {
+    if (publicRoutes.includes(req.originalUrl.split("?")[0])) {
+        return next();
+    }
+
+    return authenticateGateway(req, res, next);
+});
 app.use(authRoute);
-// app.use(express.json());
 app.use(errorMiddleware);
 
 export default app;
