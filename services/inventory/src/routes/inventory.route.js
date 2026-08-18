b19e9f5926fa716592842c961ec481984aa23337
Tonyvisko <vietthanh0135@gmail.com>
2026-08-18 21:19:47 +0700

    de cái route len tren cai route :id

--- services/inventory/src/routes/inventory.route.js
+++ services/inventory/src/routes/inventory.route.js
@@ -1,72 +1,65 @@
 import { Router } from "express";
 import { InventoryController } from "../controllers/inventory.controller.js";
-import { authenticate, authorize } from "../../../../shared/middlewares/auth.middleware.js";
-import { 
-    requireItemRequestOwnership,
-    validateApproveItemRequest,
-    validateCreateItemRequest
+import {
+  authenticate,
+  authorize,
+} from "../../../../shared/middlewares/auth.middleware.js";
+import {
+  requireItemRequestOwnership,
+  validateApproveItemRequest,
+  validateCreateItemRequest,
 } from "../middlewares/inventory.middleware.js";
 import { ROLES } from "../../../../shared/constants/roles.js";
 
 const router = Router();
 
-router.get('/health', InventoryController.health);
+router.get("/health", InventoryController.health);
 
 // get inventory list
-router.get(
-    '/',
-    authenticate(),
-    InventoryController.listInventory
-);
-
-// get inventory by id
-router.get(
-    '/:id',
-    authenticate(),
-    InventoryController.getInventoryById
-);
-
+router.get("/", authenticate(), InventoryController.listInventory);
 // get item request list
 router.get(
-    '/item-requests',
-    authenticate(),
-    authorize(ROLES.ADMIN, ROLES.MANAGER),
-    InventoryController.listRequest
+  "/item-requests",
+  authenticate(),
+  authorize(ROLES.ADMIN, ROLES.MANAGER),
+  InventoryController.listRequest,
 );
+// get inventory by id
+router.get("/:id", authenticate(), InventoryController.getInventoryById);
 
 // get item request by id
 router.get(
-    '/item-requests/:id',
-    authenticate(),
-    authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.TECHNICIAN),
-    requireItemRequestOwnership,
-    InventoryController.getRequestById
+  "/item-requests/:id",
+  authenticate(),
+  authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.TECHNICIAN),
+  requireItemRequestOwnership,
+  InventoryController.getRequestById,
 );
 
 // get item request by plan id
 router.get(
-    '/item-requests/plans/:planId',
-    authenticate(),
-    authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.TECHNICIAN),
-    InventoryController.getRequestByPlanId
+  "/item-requests/plans/:planId",
+  authenticate(),
+  authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.TECHNICIAN),
+  InventoryController.getRequestByPlanId,
 );
 
 // approve item request
 router.put(
-    '/item-requests/:id',
-    authenticate(),
-    authorize(ROLES.MANAGER),
-    validateApproveItemRequest,
-    InventoryController.approveRequest
+  "/item-requests/:id",
+  authenticate(),
+  authorize(ROLES.MANAGER),
+  validateApproveItemRequest,
+  InventoryController.approveRequest,
 );
 
 // create item request
 router.post(
-    '/item-requests',
-    authenticate(),
-    authorize(ROLES.TECHNICIAN),
-    validateCreateItemRequest,
-    InventoryController.createRequest
+  "/item-requests",
+  authenticate(),
+  authorize(ROLES.TECHNICIAN),
+  validateCreateItemRequest,
+  InventoryController.createRequest,
 );
 
 export default router;
